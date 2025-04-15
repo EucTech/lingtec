1684ac70db0b73b0cdb36cecc20caac260207262
EucTech <euccreative@gmail.com>
2025-04-15 06:36:12 -0400

    added some images

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,13 +1,15 @@
 import black_star from "./images/black-star.png";
+import yellow_star from "./images/yellow-star.png";
+import purple_star from "./images/purple-star.png";
 import person1 from "./images/person1.png";
+import logo from "./images/logo.jpg";
+import hero_img from "./images/hero-img.png";
 
-
-
-export const Images = { 
-  
-    black_star,
-      person1,
-
-
-
-}
+export const Images = {
+  black_star,
+  yellow_star,
+  purple_star,
+  person1,
+  logo,
+  hero_img,
+};
