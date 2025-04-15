91b917b47b8f493a1ff8ffb43ab3e3ba90a98b40
EucTech <euccreative@gmail.com>
2025-04-15 18:02:23 -0400

    updated

---
+++ src/utils/utilities.ts
@@ -0,0 +1,30 @@
+import { Images } from "@/assets";
+
+interface BrandProps {
+    id: number;
+    image: string;
+}
+
+export const BrandItems: BrandProps[] = [
+    {
+        id: 1,
+        image: Images.brand1.src, 
+    },
+    {
+        id: 2,
+        image: Images.brand2.src, 
+    },
+    {
+        id: 3,
+        image: Images.brand3.src, 
+    },
+    {
+        id: 4,
+        image: Images.brand4.src, 
+    },
+    {
+        id: 5,
+        image: Images.brand5.src, 
+    },
+]
+
