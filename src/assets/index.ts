a18dd41cbb01c1cb2d10f67f1bc53fb39df5fa84
EucTech <euccreative@gmail.com>
2025-04-15 18:02:02 -0400

    added images

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,15 +1,35 @@
 import black_star from "./images/black-star.png";
 import yellow_star from "./images/yellow-star.png";
 import purple_star from "./images/purple-star.png";
+import rose_star from "./images/rose-star.png";
 import person1 from "./images/person1.png";
+import person2 from "./images/person2.png";
 import logo from "./images/logo.jpg";
 import hero_img from "./images/hero-img.png";
+import brand1 from "./images/brand1.png";
+import brand2 from "./images/brand2.png";
+import brand3 from "./images/brand3.png";
+import brand4 from "./images/brand4.png";
+import brand5 from "./images/brand5.png";
+import icon1 from "./images/icon1.png";
+import icon2 from "./images/icon2.png";
+import icon3 from "./images/icon3.png";
 
 export const Images = {
   black_star,
   yellow_star,
   purple_star,
+  rose_star,
   person1,
+  person2,
   logo,
   hero_img,
+  brand1,
+  brand2,
+  brand3,
+  brand4,
+  brand5,
+  icon1,
+  icon2,
+  icon3,
 };
