af6717479c7a8a75d07e70afb00d152f99d333e6
EucTech <euccreative@gmail.com>
2025-04-16 05:36:39 -0400

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Brands from "@/components/HomePage/Brands";
 import HeroSection from "@/components/HomePage/HeroSection";
+import HowItWorks from "@/components/HomePage/HowItWorks";
+import JoinOur from "@/components/HomePage/JoinOur";
 import Testimonties from "@/components/HomePage/Testimonties";
 import WhyChoose from "@/components/HomePage/WhyChoose";
 
@@ -10,6 +12,8 @@ export default function Home() {
       <Brands/>
       <Testimonties/>
       <WhyChoose/>
+      <HowItWorks/>
+      <JoinOur/>
     </div>
   );
 }
