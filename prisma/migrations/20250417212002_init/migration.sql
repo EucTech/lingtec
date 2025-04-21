0bc933ddf14e829b9465efde0d896d2379731ecb
EucTech <euccreative@gmail.com>
2025-04-21 05:47:19 -0400

    updated

---
+++ prisma/migrations/20250417212002_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "WaitlistUser" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "fullname" TEXT NOT NULL,
+    "skill" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "WaitlistUser_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "WaitlistUser_email_key" ON "WaitlistUser"("email");
